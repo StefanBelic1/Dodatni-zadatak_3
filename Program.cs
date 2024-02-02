ba3b4dcf1c1e39455a48be6e978e23f171abefe0
Stefan Belić <127685759+StefanBelic1@users.noreply.github.com>
2024-02-02 23:08:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//ovaj obrazac je proxy, strukturni obrazac.
+
+
+    interface IImage
+    {
+        void Display();
+    }
+    class RealImage : IImage
+    {
+        private string filename;
+        public RealImage(string filename)
+        {
+            this.filename = filename;
+            LoadImageFromDisk();
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Displaying image: {filename}");
+        }
+        private void LoadImageFromDisk()
+        {
+            Console.WriteLine($"Loading image from disk: {filename}");
+        }
+    }
+    class ImageProxy : IImage
+    {
+        private string filename;
+        private RealImage image;
+        public ImageProxy(string filename)
+        {
+            this.filename = filename;
+        }
+        public void Display()
+        {
+            if (image == null)
+            {
+                image = new RealImage(filename);
+            }
+            image.Display();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+           
+            IImage image = new ImageProxy("example.jpg");   
+            image.Display();           
+            image.Display();
+        }
+    }
+
+
